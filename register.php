59b19d29f9e4e4004d98e1734da7fed715cd8df4
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-11 23:13:54 +0530

    refactored code

--- register.php
+++ register.php
@@ -6,18 +6,18 @@ require_once("includes/classes/Account.php");
 
 $account = new Account($con);
 
-if(isset($_POST["submitButton"]))
-{
-$firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
-$lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
-$username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
-$email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
-$email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
-$password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
-$password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+if(isset($_POST["submitButton"])){
 
-$account->register($firstName, $lastName, $username, $email , $email2, $password, $password2);
-}
+        $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
+        $lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        $email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+        $email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
+        $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+        $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+
+       
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,7 +37,7 @@ $account->register($firstName, $lastName, $username, $email , $email2, $password
        <span>To Continue to Kingflix</span>
       
    </div>
-   <form method="post">
+   <form method="POST">
 
        <?php echo $account->getError(Constants::$firstNameCharacters); ?>
        <input type="text" name="firstName" placeholder="First Name" required>
